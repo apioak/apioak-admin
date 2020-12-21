90cf2d549406e505228649cba450c69927615bd4
Janko <shuaijinchao@gmail.com>
2020-12-22 00:02:16 +0800

    feature: update project struct and add default config file.

--- Makefile
+++ Makefile
@@ -3,8 +3,9 @@
 build:
 	@echo "Building APIOAK"
 	@mkdir -p bin etc
+	@cp conf/apioak.yaml etc/apioak.yaml
 	@go build -o bin/apioak .
 
 clean:
 	@echo "Remove APIOAK"
-	@rm -f bin/*
+	@rm -rf bin etc
--- apioak.go
+++ apioak.go
@@ -1,54 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
-	"strconv"
-)
-
-type Upstream struct {
-	host string
-	port string
-}
-
-func (upstream *Upstream) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	remote, err := url.Parse("http://" + upstream.host + ":" + upstream.port)
-	if err != nil {
-		panic(err)
-	}
-	proxy := httputil.NewSingleHostReverseProxy(remote)
-	proxy.ServeHTTP(writer, request)
-}
-
-func startServer(application *ConfigApp) {
-	u := &Upstream{"127.0.0.1", "10222"}
-	err := http.ListenAndServe(application.Host+":"+strconv.Itoa(application.Port), u)
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
+import "github.com/apioak/apioak-go/src"
 
 func main() {
-	flag.Parse()
-
-	if err := initConfig(); err != nil {
-		panic(err)
-	}
-
-	if config.CLI.Version {
-		fmt.Printf("APIOAK: Version %s\n", AppVersion)
-		os.Exit(1)
-	}
-
-	if len(config.EtcdNodes) == 0 {
-		fmt.Print("error: config etcd nodes is empty")
-		os.Exit(1)
-	}
-
-	startServer(&config.App)
+	src.Main()
 }
