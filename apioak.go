863aeb54acc8aaa709435caa738e17386250b8c3
Janko <shuaijinchao@gmail.com>
2020-12-27 16:54:54 +0800

    change: add test channel code.

--- apioak.go
+++ apioak.go
@@ -1,7 +1,28 @@
 package main
 
-import "github.com/apioak/apioak-go/src"
+import (
+	"fmt"
+	"time"
+)
+
+func produce(p chan<- int) {
+	for i := 0; i < 10; i++ {
+		p <- i
+		fmt.Println(" send: ", i)
+	}
+}
+
+func consumer(c <-chan int) {
+	for i := 0; i < 10; i++ {
+		v := <-c
+		fmt.Println("receive: ", v)
+	}
+}
 
 func main() {
-	src.Main()
+	// src.Main()
+	ch := make(chan int, 10)
+	go produce(ch)
+	go consumer(ch)
+	time.Sleep(1 * time.Second)
 }
